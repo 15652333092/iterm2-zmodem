e42cbe5535ace9aee4a237174460f591ac9b62b2
Magic <72803980+15652333092@users.noreply.github.com>
2025-02-28 15:08:47 +0800

    Update iterm2-recv-zmodem.sh

--- iterm2-recv-zmodem.sh
+++ iterm2-recv-zmodem.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
-# Author: Matt Mastracci (matthew@mastracci.com)
-# AppleScript from http://stackoverflow.com/questions/4309087/cancel-button-on-osascript-in-a-bash-script
-# licensed under cc-wiki with attribution required 
-# Remainder of script public domain
 
-#FILE=`osascript -e 'tell application "iTerm" to activate' -e 'tell application "iTerm" to set thefile to choose folder with prompt "Choose a folder to place received files in"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")"`
-FILE="/Users/"`whoami`"/Downloads"
+FILE=`osascript -e 'tell application "iTerm" to activate' -e 'tell application "iTerm" to set thefile to choose folder with prompt "Choose a folder to place received files in"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")"`
+# FILE="/Users/"`whoami`"/Downloads"
 if [[ $FILE = "" ]]; then
 	echo Cancelled.
 	# Send ZModem cancel
 	echo -e \\x18\\x18\\x18\\x18\\x18
-	echo "# Cancelled transfer       "
-	/usr/local/bin/growlnotify -a /Applications/iTerm.app -n "iTerm" -m 'Cancelled.' -t "File transfer"
+	sleep 1
 	echo
+	echo \# Cancelled transfer
 else
-	echo $FILE
-	cd "${FILE}"
-	/usr/local/bin/growlnotify -a /Applications/iTerm.app -n "iTerm" -m 'Transfer started...' -t "File transfer"
-	/usr/local/bin/rz
-	/usr/local/bin/growlnotify -a /Applications/iTerm.app -n "iTerm" -m 'Transfer finished.' -t "File transfer"
-	echo "# Transfer finished        "
+    echo $FILE
+	cd "$FILE"
+	/opt/homebrew/bin/rz --rename --escape --binary --bufsize 4096 
+	sleep 1
 	echo
+	echo \# Sent \-\> $FILE
 fi
